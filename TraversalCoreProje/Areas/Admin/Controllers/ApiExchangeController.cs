1e110d6aaa9e1a4d8da89545e800751b4c4c2528
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-07-27 07:21:07 +0300

    film ve kur  rapid apileri eklendi

---
+++ TraversalCoreProje/Areas/Admin/Controllers/ApiExchangeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<BookingExchangeViewModel2> bookingExchange = new List<BookingExchangeViewModel2>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+                Headers =
+    {
+        { "x-rapidapi-key", "06272128e4msha438ef5f41c84c8p1badccjsn3f82930d95d3" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<BookingExchangeViewModel2>(body);
+                return View(values.exchange_rates);
+            }
+        }
+    }
+}
